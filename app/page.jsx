6de27813c067081edba8669788607cab7c96cd43
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-26 14:04:32 +0530

    react server components

---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>Hello Server Component</h1>
+    );
+}
---
+++ build/page.js
@@ -0,0 +1,7 @@
+// app/page.jsx
+function Page() {
+  return /* @__PURE__ */ React.createElement("h1", null, "Hello Server Component");
+}
+export {
+  Page as default
+};
