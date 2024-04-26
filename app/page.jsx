2ac0ad8c9b380d5cf01eb1949cb03673281bd62d
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-26 16:09:27 +0530

    adding server streaming and bundling the stream to browser

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,31 @@
+import { getAll } from "../data/db.js";
+import { Suspense } from 'react';
+
 export default function Page() {
     return (
-        <h1>Hello Server Component</h1>
+        <>
+            <h1 className='text-3xl mb-3'>Spotifn't</h1>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Albums />
+            </Suspense>
+        </>
+
+    );
+}
+
+async function Albums() {
+    const albums = await getAll();
+    return (
+        <ul>
+            {albums.map((a) => (
+                <li key={a.id} className='flex gap-2 items-center mb-2'>
+                    <img className="w-20 aspect-sqaure" src={a.cover} alt={a.title} />
+                    <div>
+                        <h3 className="text-xl">{a.title}</h3>
+                        <p>{a.songs.length} songs</p>
+                    </div>
+                </li>
+            ))}
+        </ul>
     );
 }
--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 import {serve} from "@hono/node-server";
 import { Hono } from "hono";
 import { build as esbuild} from "esbuild";
-import { fileURLToPath } from "node:url";
+import { fileURLToPath } from 'node:url';
 import { renderToString } from "react-dom/server";
 import { createElement } from "react";
+import * as ReactServerDom from "react-server-dom-webpack/server.browser";
+import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = new Hono();
+
 app.get("/", async (c) => {
+    return c.html(`
+        <!DOCTYPE html>
+            <html>
+            <head>
+                <title>React Server Components from Scratch</title>
+                <script src="https://cdn.tailwindcss.com"></script>
+            </head>
+            <body>
+                <div id="root"></div>
+                <script type="module" src="/build/_client.js"></script>
+            </body>
+	</html>
+    `);
+});
+
+app.use("/build/*", serveStatic());
+
+app.get("/rsc", async (c) => {
     const Page = await import("./build/page.js");
-    const html = renderToString(createElement(Page.default));
-    return c.html(html);
+    const stream = ReactServerDom.renderToReadableStream(createElement(Page.default));
+    return new Response(stream);
 });
 
 serve(app, async (info) => {
@@ -26,7 +47,17 @@ async function build() {
         outdir: resolveBuild(),
         //avoid bunding  npm pagckages
         packages: "external"
-    })
+    });
+
+    await esbuild ({
+        bundle: true,
+        format: "esm",
+        logLevel: "error",
+        entryPoints: [resolveApp("_client.jsx")],
+        outdir: resolveBuild(),
+        splitting: true,
+        plugins: []
+    });
 }
 
 const appDir = new URL("./app/", import.meta.url);
